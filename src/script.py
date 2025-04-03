d0a1abdd77cd7814ac69292d5338b9518ebf6e72
Corey <coreyg142@gmail.com>
2025-04-03 12:41:45 -0400

    A bit of clean up

--- src/script.py
+++ src/script.py
@@ -20,8 +20,6 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-logger.info("Starting script...")
-logger.info("Loading environment variables...")
 def signal_handler(sig, frame):
     logger.info("Exiting...")
     sys.exit(0)
@@ -29,18 +27,44 @@ def signal_handler(sig, frame):
 signal.signal(signal.SIGINT, signal_handler)
 signal.signal(signal.SIGTERM, signal_handler)
 
+logger.info("Starting script...")
+logger.info("Loading environment variables...")
 base_url = "https://api.steampowered.com/IPhysicalGoodsService/CheckInventoryAvailableByPackage/v1"
 webhook_url = os.getenv("webhook_url")
 key = os.getenv("key")
 country_code = os.getenv("country_code")
-check_interval = os.getenv("check_interval", 60)
+check_interval = int(os.getenv("check_interval", 60))
+send_webhooks = os.getenv("send_webhooks", "true").lower() == "true"
 
 def send_webhook_message(message):
-    webhook = DiscordWebhook(
-        url=webhook_url, 
-        content=message
-    )
-    webhook.execute()
+    if send_webhooks:
+        webhook = DiscordWebhook(
+            url=webhook_url, 
+            content=message
+        )
+        webhook.execute()
+
+def check_availability_device(device):
+    status_changed = False
+    package_id = device["packageId"]
+    querystring = {
+        "key": key, "packageId": package_id, "country_code": country_code
+    }
+    try:
+        response = requests.get(base_url, params=querystring)
+        response.raise_for_status()  # Raise an error for bad responses
+    except requests.exceptions.RequestException as e:
+        logger.error(f"Error fetching data for package ID {package_id}: {e}")
+        send_webhook_message(f"Error fetching data for package ID\n{e}")
+        return
+    response_json = response.json()
+
+    if response_json["response"]["inventory_available"] != device["available"]:
+        device["available"] = response_json["response"]["inventory_available"]
+        status_changed = True
+
+    return status_changed
+
 
 def check_availability():
     logger.info("Checking availability...")
@@ -50,42 +74,27 @@ def check_availability():
     try:
         with open("config/devices.json", "r") as f:
             devices = json.load(f)
-            webhookContent = ""
-            for device in devices:
-                status_changed = False
-                package_id = device["packageId"]
-                querystring = {
-                    "key": key, "packageId": package_id, "country_code": country_code
-                }
-                try:
-                    response = requests.get(base_url, params=querystring)
-                    response.raise_for_status()  # Raise an error for bad responses
-                except requests.exceptions.RequestException as e:
-                    logger.error(f"Error fetching data for package ID {package_id}: {e}")
-                    send_webhook_message(f"Error fetching data for package ID\n{e}")
-                    return
-                response_json = response.json()
+        webhookContent = ""
+        for device in devices:
+            status_changed = check_availability_device(device)
+            if status_changed:
+                webhookContent += f"{device['capacity']} {"OLED" if device['OLED'] else "LCD"} {"is available" if device['available'] else "is not available"}\n"
+                with open("config/devices.json", "w") as f:
+                    json.dump(devices, f, indent=2)
 
-                if response_json["response"]["inventory_available"] != device["available"]:
-                    device["available"] = response_json["response"]["inventory_available"]
-                    status_changed = True
-
-                if status_changed:
-                    with open("config/devices.json", "w") as f:
-                        json.dump(devices, f, indent=2)
-                    webhookContent += f"{device['capacity']} {"OLED " if device['OLED'] else "LCD"} {"is available" if device['available'] else "is not available"}\n"
-            if webhookContent != "":
-                logger.info(webhookContent)
-                send_webhook_message(webhookContent)
-            else:
-                logger.info("No changes in availability status.")
-    except Exception:
+        if webhookContent != "":
+            logger.info(webhookContent)
+            send_webhook_message(webhookContent)
+        else:
+            logger.info("No changes in availability status.")
+    except Exception as e:
         logger.error(f"Error running script: {traceback.format_exc()}")
         send_webhook_message(f"An error occurred while running the script.\n{traceback.format_exc()}")
+        sys.exit(1)
 
 if __name__ == "__main__":
     check_availability()
-    time.sleep(check_interval)  # Check every 60 seconds
+    time.sleep(check_interval)  # Check every x seconds
     while True:
         check_availability()
-        time.sleep(check_interval)  # Check every 60 seconds
+        time.sleep(check_interval)  # Check every x seconds
